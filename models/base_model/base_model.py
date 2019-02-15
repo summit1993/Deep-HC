8e104d9ac7fa0887cadc7bf4e4f9e6966671e771
summit1993 <changdong_xu@163.com>
2019-02-15 22:59:05 +0800

    change the resnet to extract features

---
+++ models/base_model/base_model.py
@@ -0,0 +1,12 @@
+from backbone.resnet import *
+
+switch={'resnet-18':lambda num_classes:resnet18(pretraine=True, num_classes=num_classes),
+        'resnet-34': lambda num_classes:resnet34(True, num_classes=num_classes),
+        'resnet-50':lambda num_classes:resnet50(True, num_classes=num_classes),
+        'resnet-101':lambda num_classes:resnet101(True, num_classes=num_classes),
+        'resnet-152': lambda num_classes: resnet152(True, num_classes=num_classes)}
+
+class Base_Model:
+    def __init__(self, back_bone_name='resnet-101', num_classes=-1):
+        self.back_bone = switch[back_bone_name](num_classes)
+        # here to go
