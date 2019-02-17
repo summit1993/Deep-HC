2ea2f99dedd525003d126c3e1231aae5d643ce1f
summit1993 <changdong_xu@163.com>
2019-02-17 09:57:33 +0800

    fix issue of metrics

--- models/utilities/my_metrics.py
+++ models/utilities/my_metrics.py
@@ -2,7 +2,7 @@
 
 def MAE_sum(true_labels, predictions):
     error_sum = sum(abs(true_labels - predictions))
-    return error_sum * 1.0
+    return error_sum.float()
 
 def MAE_mean(true_labels, predictions):
     error_sum = MAE_sum(true_labels, predictions)
